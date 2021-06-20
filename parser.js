5d5dde192f5702382cb4059e54ba725e0c4b1cc2
gremliiin <rekufo@gmail.com>
2021-06-20 19:25:05 +0600

    refactor

--- parser.js
+++ parser.js
@@ -1,35 +1,31 @@
 const fs = require("fs");
-let txt = fs.readFileSync("e-designations.csv", "utf8");
+let txt = fs.readFileSync("chord-progressions.csv", "utf8");
 
 const cvsParser = (flag = '', sep = ';', data = '') => {
     let strings = [];
     let row = [];
     let result = [];
-    let error = [];
+    let error = '';
 
+    //Создание массива строк
     strings = data.split("\n");
-
-    strings = strings.map(el => {
-        return el.replace('\r', '');
-    });
+    //Если файл пустой
+    if( data === '') {
+        return error = "empty file";
+    } else if (strings.length === 1 && flag.trim() != '') {
+        return error = "empty file";
+    }
     
+    //удаление лишнего в строках
     strings = strings.map(el => {
+        el = el.trim();
         if(el[el.length - 1] === sep) {
-            let arr = [];
-            arr = el.split('');
-            arr.pop();
-            el = arr.join('');
+            el = el.substring(0, el.length - 1);
         }
-
         return el;
     }); 
 
-    if( data === '') {
-        error.push("empty file");
-    } else if (strings.length === 1 && flag.trim() != '') {
-        error.push("empty file");
-    }
-
+    //если флаг пустой, то заголовком будет являться 1 строка файла
     if (flag.trim() === ''){
         flag = strings.shift();
         flag = flag.split(sep);
@@ -37,60 +33,40 @@ const cvsParser = (flag = '', sep = ';', data = '') => {
         flag = flag.split(sep);
     }
 
-
+    //разбиение строк на ряды
     for(let i = 0; i < strings.length; i++){
         row.push(strings[i].split(sep));
         if(flag.length < strings[i].split(sep).length || flag.some(el => el.trim() === '')) {
-            error.push("the size of the header does not match the size of the table");
-            break;
+            return error = "the size of the header does not match the size of the table";
         }
     }
     
+    //Создание объекта с приведением типов
     row.forEach(el => {
         let obj = {};
 
         for(let i = 0; i < el.length; i++) {
             if(String(el[i].trim()) == ''){
-                el[i] = 'empty';
+                el[i] = 'NULL';
             }
-        }
-
-        for(let i = 0; i < el.length; i++) {
-            if(!isNaN(Number(el[i]) && el[i] != null )){
+            if(!isNaN(Number(el[i]))){
                 el[i] = Number(el[i]);
             }
-        }
 
-        for(let i = 0; i < el.length; i++) {
             if(String(el[i]).toLowerCase() === 'false'){
                 el[i] = false;
             } else if(String(el[i]).toLowerCase() === 'true') {
                 el[i] = true;
-            }
-        }
-
-        for(let i = 0; i < el.length; i++) {
-            if(String(el[i]) === 'empty'){
-                el[i] = null;
             } else if(String(el[i]).toLowerCase() === 'null'){
                 el[i] = null;
             } else if(String(el[i]).toLowerCase() === 'undefined') {
                 el[i] = undefined;
             }
-        }
-
-        for(let i = 0; i < el.length; i++) {
             obj[flag[i]] = el[i];
         }
-
         result.push(obj);
     })
-
-    if(error.length === 0) {
-        return result;
-    } else {
-        return error;
-    }
+    return result;
 }
 
 console.log(cvsParser('', ',', txt));
